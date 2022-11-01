9ae384a0fbb122ffed58eab321fa2f218725b70f
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2022-11-01 15:17:11 +0100

    setup project

---
+++ track_element/src/lib.rs
@@ -0,0 +1,48 @@
+pub struct Message {
+    message: String,
+}
+
+impl Message {
+    pub fn new(message: String) -> Self {
+        Self { message }
+    }
+
+    fn print(&self) {
+        println!("{}", self.message)
+    }
+}
+
+pub trait TrackElement {
+    type State;
+
+    fn state(&self) -> &Self::State;
+    fn set_state(&mut self, new_state: Self::State);
+    fn send_message(&self, message: Message);
+}
+
+#[derive(Debug)]
+pub enum PointState {
+    Left,
+    Right,
+}
+
+pub struct Point {
+    state: PointState,
+}
+
+impl TrackElement for Point {
+    type State = PointState;
+
+    fn state(&self) -> &Self::State {
+        &self.state
+    }
+
+    fn set_state(&mut self, new_state: Self::State) {
+        self.state = new_state;
+        println!("Point state is now {:?}", self.state);
+    }
+
+    fn send_message(&self, message: Message) {
+        message.print()
+    }
+}
---
+++ track_element/target/debug/deps/track_element-89260141ca565ac5.d
@@ -0,0 +1,5 @@
+/home/clemens/Studium/MA/MP/codegen/track_element/target/debug/deps/track_element-89260141ca565ac5.rmeta: src/lib.rs
+
+/home/clemens/Studium/MA/MP/codegen/track_element/target/debug/deps/track_element-89260141ca565ac5.d: src/lib.rs
+
+src/lib.rs:
