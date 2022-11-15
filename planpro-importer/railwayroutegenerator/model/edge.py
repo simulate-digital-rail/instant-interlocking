8c7fd6ae6ec46e120b2503ba7b7802e9b8b5492b
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2022-11-15 10:33:56 +0100

    remove railwayroutegenerator

--- planpro-importer/railwayroutegenerator/model/edge.py
+++
@@ -1,8 +0,0 @@
-class Edge(object):
-
-    def __init__(self, uuid, node_a, node_b, length):
-        self.uuid = uuid
-        self.node_a = node_a
-        self.node_b = node_b
-        self.signals = []
-        self.length = length
