76226bbba72b02018e4917e59c4919229de1a2da
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2022-11-17 09:58:28 +0100

    remove unused dependencies and fix tests

--- track_element/examples/driveway.rs
+++ track_element/examples/driveway.rs
@@ -1,22 +1,32 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 use track_element::{
-    TrackElement,
     driveway::{Driveway, TargetState},
     point::{Point, PointState},
+    signal::{Signal, SignalState, SignalType},
+    TrackElement,
 };
 
 fn main() {
-    let p1 = Rc::new(RefCell::new(Point::new(PointState::Left, "A".to_owned())));
-    let p2 = Rc::new(RefCell::new(Point::new(PointState::Left, "B".to_owned())));
+    let p1 = Rc::new(RefCell::new(Point::new(PointState::Left, "P1".to_string())));
+    let p2 = Rc::new(RefCell::new(Point::new(PointState::Left, "P2".to_string())));
+    let s = Rc::new(RefCell::new(Signal::new(
+        SignalState::default(),
+        SignalType::ToDo,
+        "S".to_string(),
+    )));
 
     let ts = TargetState::new(
-        vec![(p1.clone(), PointState::Right), (p2.clone(), PointState::Left)],
-        vec![],
+        vec![
+            (p1.clone(), PointState::Right),
+            (p2.clone(), PointState::Left),
+        ],
+        vec![(s.clone(), SignalState::Ks1)],
     );
 
-    let mut dw = Driveway::new(Vec::new(), ts);
+    let mut dw = Driveway::new(Vec::new(), ts, s.clone(), s.clone());
     dw.set_way().unwrap();
     assert!(matches!(p1.borrow().state(), PointState::Right));
     assert!(matches!(p2.borrow().state(), PointState::Left));
+    assert!(matches!(s.borrow().state(), SignalState::Ks1));
 }
--- track_element/src/test.rs
+++ track_element/src/test.rs
@@ -1,9 +1,12 @@
-use std::cell::RefCell;
 use std::rc::Rc;
+use std::{borrow::Borrow, cell::RefCell};
 
 use crate::{
+    driveway::Driveway,
+    driveway::TargetState,
     point::{Point, PointState},
-    TrackElement, driveway::TargetState, driveway::Driveway,
+    signal::{Signal, SignalState, SignalType},
+    TrackElement,
 };
 
 #[test]
@@ -16,16 +19,42 @@ fn set_point() {
 
 #[test]
 fn set_basic_driveway() {
-    let p1 = Rc::new(RefCell::new(Point::new(PointState::Left, String::new())));
-    let p2 = Rc::new(RefCell::new(Point::new(PointState::Left, String::new())));
+    let p1 = Rc::new(RefCell::new(Point::new(PointState::Left, "P1".to_string())));
+    let p2 = Rc::new(RefCell::new(Point::new(PointState::Left, "P2".to_string())));
+    let s = Rc::new(RefCell::new(Signal::new(
+        SignalState::default(),
+        SignalType::ToDo,
+        "S".to_string(),
+    )));
 
     let ts = TargetState::new(
-        vec![(p1.clone(), PointState::Right), (p2.clone(), PointState::Left)],
-        vec![],
+        vec![
+            (p1.clone(), PointState::Right),
+            (p2.clone(), PointState::Left),
+        ],
+        vec![(s.clone(), SignalState::Ks1)],
     );
 
-    let mut dw = Driveway::new(Vec::new(), ts);
+    let mut dw = Driveway::new(Vec::new(), ts, s.clone(), s.clone());
     dw.set_way().unwrap();
-    assert!(matches!(p1.borrow().state(), PointState::Right));
-    assert!(matches!(p2.borrow().state(), PointState::Left));
+
+    // These types are only needed in test cases like this - They do not appear in the actual generated code.
+    assert!(matches!(
+        <Rc<RefCell<Point>> as Borrow<RefCell<Point>>>::borrow(&p1)
+            .borrow()
+            .state(),
+        PointState::Right
+    ));
+    assert!(matches!(
+        <Rc<RefCell<Point>> as Borrow<RefCell<Point>>>::borrow(&p2)
+            .borrow()
+            .state(),
+        PointState::Left
+    ));
+    assert!(matches!(
+        <Rc<RefCell<Signal>> as Borrow<RefCell<Signal>>>::borrow(&s)
+            .borrow()
+            .state(),
+        SignalState::Ks1
+    ));
 }
