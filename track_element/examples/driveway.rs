9f0071c21f0136af9635c4efee26dbb9d3bafc4c
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2022-11-17 10:02:12 +0100

    fix test and example

--- track_element/examples/driveway.rs
+++ track_element/examples/driveway.rs
@@ -3,19 +3,21 @@ use std::cell::RefCell;
 use track_element::{
     TrackElement,
     driveway::{Driveway, TargetState},
-    point::{Point, PointState},
+    point::{Point, PointState}, signal::{Signal, SignalState, SignalType},
 };
 
 fn main() {
     let p1 = Rc::new(RefCell::new(Point::new(PointState::Left, "A".to_owned())));
     let p2 = Rc::new(RefCell::new(Point::new(PointState::Left, "B".to_owned())));
-
+    let s1 = Rc::new(RefCell::new(Signal::new(SignalState::Hp0, SignalType::ToDo, "A".to_string())));
+    let s2 = Rc::new(RefCell::new(Signal::new(SignalState::Hp0, SignalType::ToDo, "B".to_string())));
+    
     let ts = TargetState::new(
         vec![(p1.clone(), PointState::Right), (p2.clone(), PointState::Left)],
         vec![],
     );
 
-    let mut dw = Driveway::new(Vec::new(), ts);
+    let mut dw = Driveway::new(Vec::new(), ts, s1, s2);
     dw.set_way().unwrap();
     assert!(matches!(p1.borrow().state(), PointState::Right));
     assert!(matches!(p2.borrow().state(), PointState::Left));
--- track_element/src/test.rs
+++ track_element/src/test.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 
 use crate::{
     point::{Point, PointState},
-    TrackElement, driveway::TargetState, driveway::Driveway,
+    TrackElement, driveway::TargetState, driveway::Driveway, signal::{Signal, SignalState, SignalType},
 };
 
 #[test]
@@ -18,13 +18,15 @@ fn set_point() {
 fn set_basic_driveway() {
     let p1 = Rc::new(RefCell::new(Point::new(PointState::Left, String::new())));
     let p2 = Rc::new(RefCell::new(Point::new(PointState::Left, String::new())));
-
+    let s1 = Rc::new(RefCell::new(Signal::new(SignalState::Hp0, SignalType::ToDo, "A".to_string())));
+    let s2 = Rc::new(RefCell::new(Signal::new(SignalState::Hp0, SignalType::ToDo, "B".to_string())));
+    
     let ts = TargetState::new(
         vec![(p1.clone(), PointState::Right), (p2.clone(), PointState::Left)],
         vec![],
     );
 
-    let mut dw = Driveway::new(Vec::new(), ts);
+    let mut dw = Driveway::new(Vec::new(), ts, s1, s2);
     dw.set_way().unwrap();
     assert!(matches!(p1.borrow().state(), PointState::Right));
     assert!(matches!(p2.borrow().state(), PointState::Left));
