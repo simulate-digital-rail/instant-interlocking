96c7c825c2a17709569ba2f69d0db08b758cade7
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2023-03-14 15:08:44 +0100

    add better error messages for python code

--- ui/app.py
+++ ui/app.py
@@ -37,7 +37,11 @@ def run_converter():
     polygon = request.args.get("polygon")
     if not polygon:
         return "No location specified", 400
-    topology = ORMImporter().run(polygon)
+    
+    try:
+        topology = ORMImporter().run(polygon)
+    except Exception as e:
+        return f"Exception while generating topology: `{e}`.\nThis probably means you need to tweak your selected polygon.", 500
 
     # persist to database
     query_db("INSERT INTO interlockings (title) VALUES ('test');")
@@ -45,12 +49,15 @@ def run_converter():
 
     # export JSON files for interlocking
     exporter = InterlockingExporter(topology)
-    with open(f"generated/{rowid}_routes.json", "w") as routes_file:
-        json.dump(exporter.export_routes(), routes_file)
-    with open(f"generated/{rowid}_topology.json", "w") as topology_file:
-        json.dump(exporter.export_topology(), topology_file)
-    with open(f"generated/{rowid}_placement.json", "w") as placement_file:
-        json.dump(exporter.export_placement(), placement_file)
+    try:
+        with open(f"generated/{rowid}_routes.json", "w") as routes_file:
+            json.dump(exporter.export_routes(), routes_file)
+        with open(f"generated/{rowid}_topology.json", "w") as topology_file:
+            json.dump(exporter.export_topology(), topology_file)
+        with open(f"generated/{rowid}_placement.json", "w") as placement_file:
+            json.dump(exporter.export_placement(), placement_file)
+    except Exception as e:
+        return f"Exception while generating JSON files: `{e}`.\nThis probably means you need to tweak your selected polygon.", 500
 
     # start build process
     thread = threading.Thread(target=generate_interlocking, args=(rowid,))
